c12fe2e247e985e84d4e4af73b304ef55e13d488
giorgiobazzo <giorgiobazzo@gmail.com>
2020-01-29 18:04:16 -0300

    Versao 2

--- TesteGit.dpr
+++ TesteGit.dpr
@@ -2,7 +2,8 @@ program TesteGit;
 
 uses
   Vcl.Forms,
-  Main in 'Main.pas' {Form6};
+  Main in 'Main.pas' {Form6},
+  Unit1 in 'Forms\Unit1.pas' {Form1};
 
 {$R *.res}
 
@@ -10,5 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm6, Form6);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
